e9535271020cdbad96e8179b0d5610e1667d82a0
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-06-24 17:31:17 +0300

    chabged dialogs styles

--- src/pages/Dialogs/Dialogs.js
+++ src/pages/Dialogs/Dialogs.js
@@ -22,6 +22,72 @@ const Dialogs = () => {
                   <div className={s.name}>Alex Maximov</div>
                 </a>
               </li>
+              <li className={`${s.item} ${s.active}`}>
+                <a href="#" className={s.link}>
+                  <div className={s.avatar}>
+                    <img
+                      src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className={s.name}>Alex Maximov</div>
+                </a>
+              </li>
+              <li className={s.item}>
+                <a href="#" className={s.link}>
+                  <div className={s.avatar}>
+                    <img
+                      src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className={s.name}>Alex Maximov</div>
+                </a>
+              </li>
+              <li className={s.item}>
+                <a href="#" className={s.link}>
+                  <div className={s.avatar}>
+                    <img
+                      src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className={s.name}>Alex Maximov</div>
+                </a>
+              </li>
+              <li className={s.item}>
+                <a href="#" className={s.link}>
+                  <div className={s.avatar}>
+                    <img
+                      src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className={s.name}>Alex Maximov</div>
+                </a>
+              </li>
+              <li className={s.item}>
+                <a href="#" className={s.link}>
+                  <div className={s.avatar}>
+                    <img
+                      src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className={s.name}>Alex Maximov</div>
+                </a>
+              </li>
+              <li className={s.item}>
+                <a href="#" className={s.link}>
+                  <div className={s.avatar}>
+                    <img
+                      src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className={s.name}>Alex Maximov</div>
+                </a>
+              </li>
               <li className={s.item}>
                 <a href="#" className={s.link}>
                   <div className={s.avatar}>
@@ -60,9 +126,39 @@ const Dialogs = () => {
 
           <div className={s.chat}>
             <ul className={s.chat_list}>
-              <li className={s.chat_item}>hey, how are you?</li>
-              <li className={s.chat_item}>hey, how are you?</li>
-              <li className={`${s.chat_item} ${s.me}`}>hey, how are you?</li>
+              <li className={`${s.chat_item} ${s.notMy}`}>hey, how are you?</li>
+              <li className={`${s.chat_item} ${s.notMy}`}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Delectus, minima. Sequi, ea atque. Repellat mollitia nostrum
+                ducimus esse illo numquam soluta quaerat labore laborum,
+                consequatur ratione commodi. Ex, molestiae dicta?
+              </li>
+              <li className={`${s.chat_item} ${s.my}`}>
+                Sequi, ea atque. Repellat mollitia nostrum ducimus esse illo
+                numquam soluta quaerat labore laborum, consequatur ratione
+                commodi. Ex, molestiae dicta?
+              </li>
+              <li className={`${s.chat_item} ${s.my}`}>
+                Sequi, ea atque. Repellat mollitia nostrum ducimus esse illo
+                numquam soluta quaerat labore laborum, consequatur ratione
+                commodi. Ex, molestiae dicta?
+              </li>
+              <li className={`${s.chat_item} ${s.notMy}`}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Delectus, minima. Sequi, ea atque. Repellat mollitia nostrum
+                ducimus esse illo numquam soluta quaerat labore laborum,
+                consequatur ratione commodi. Ex, molestiae dicta?
+              </li>
+              <li className={`${s.chat_item} ${s.my}`}>
+                Sequi, ea atque. Repellat mollitia nostrum ducimus esse illo
+                numquam soluta quaerat labore laborum, consequatur ratione
+                commodi. Ex, molestiae dicta?
+              </li>
+              <li className={`${s.chat_item} ${s.my}`}>
+                Sequi, ea atque. Repellat mollitia nostrum ducimus esse illo
+                numquam soluta quaerat labore laborum, consequatur ratione
+                commodi. Ex, molestiae dicta?
+              </li>
             </ul>
           </div>
         </div>
